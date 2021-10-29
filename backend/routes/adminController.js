0c4c68b99c61dd2d7e8e36a0b745105572057d83
doniasharaf <doniasharaf18@gmail.com>
2021-10-29 21:27:53 +0200

    admin controller

--- backend/routes/adminController.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("HI MAROOMA HOB HAYATY ");
-});
-
-module.exports = router;
