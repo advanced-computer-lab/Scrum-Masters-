2f45551938e7f9b401b1367ba5d5d5cf313eb515
doniasharaf <doniasharaf18@gmail.com>
2021-10-29 17:07:32 +0200

    app.js initialization

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 8081;
+
+//DB CONNECTION
+
+mongoose
+  .connect(process.env.DB_CONNECTION_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => console.log("DB connected"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+//admin/ ROUTES
+
+app.use("/admin", require("./routes/admin/adminController"));
+
+
+app.listen(port, () => {
+  console.log("App is listening at port: ", port);
+});
