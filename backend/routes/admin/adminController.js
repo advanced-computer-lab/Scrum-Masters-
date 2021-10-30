eb7bf06cb88d22033712d2f470eb26601fa3d39b
maram <marambenamer@gmail.com>
2021-10-30 22:50:09 +0200

    flight creation

---
+++ backend/routes/admin/adminController.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const Flight = require("../../Models/Flight");
+
+router.post("/search", async (req, res) => {
+  const criteria = req.body;
+  console.log(criteria);
+  try {
+    const query = await Flight.find(criteria);
+    console.log(query);
+    res.json(query);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+router.post("/createflight",async(req, res)=>{
+const flight=new Flight({flightNumber:req.params.flightNumber});
+try{
+  await flight.save();
+  res.send("inserted dataaaaa");
+}
+catch(err){
+  console.log(err);
+}
+
+});
+
+
+router.patch("/update/:id", async (req, res) => {
+  Flight.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((result) => {
+      //new:true returns modified document not original
+      res.send(result);
+      console.log(result);
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    });
+});
+
+module.exports = router;
