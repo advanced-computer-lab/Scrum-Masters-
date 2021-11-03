e062b0ba77e7c05d7d4addd5274c68e6fbd47b5b
ahmedserryy <serry1393@gmail.com>
2021-11-03 23:24:38 +0200

    ViewFlights Pagination done

--- frontend/src/components/admin/FlightCard.js
+++ frontend/src/components/admin/FlightCard.js
@@ -9,6 +9,7 @@ import {
   TableHead,
   TableRow,
   Typography,
+  Tooltip,
 } from '@material-ui/core';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
@@ -20,15 +21,17 @@ const FlightCard = (props) => {
   const [open, setOpen] = React.useState(false);
   return (
     <React.Fragment>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+      <TableRow sx={{ '& > *': {} }}>
         <TableCell>
-          <IconButton
-            aria-label='expand row'
-            size='small'
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
+          <Tooltip title='More Details'>
+            <IconButton
+              aria-label='expand row'
+              size='small'
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+          </Tooltip>
         </TableCell>
         <TableCell component='th'>{row.flightNumber}</TableCell>
         <TableCell align='left'>{row.departureAirport}</TableCell>
@@ -37,40 +40,42 @@ const FlightCard = (props) => {
         <TableCell align='center'>{row.departureDate}</TableCell>
         <TableCell align='left'>{row.arrivalDate}</TableCell>
         <TableCell align='center'>
-          <IconButton aria-label='edit'>
-            <EditIcon />
-          </IconButton>
-          <IconButton aria-label='delete'>
-            <DeleteButton />
-          </IconButton>
+          <Tooltip title='Edit'>
+            <IconButton aria-label='edit' id={row._id}>
+              <EditIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title='Delete'>
+            <IconButton aria-label='delete' id={row._id}>
+              <DeleteButton />
+            </IconButton>
+          </Tooltip>
         </TableCell>
       </TableRow>
       <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={100}>
           <Collapse in={open} timeout='auto' unmountOnExit>
             <Box sx={{ margin: 1 }}>
               <Typography variant='h6' gutterBottom component='div'>
                 Details
               </Typography>
-              <Table size='small' aria-label='purchases'>
+              <Table>
                 <TableHead>
                   <TableRow>
-                    <TableCell>Economy Seats</TableCell>
-                    <TableCell>Business Seats</TableCell>
+                    <TableCell align='center'>Economy Seats</TableCell>
+                    <TableCell align='center'>Business Seats</TableCell>
                     <TableCell align='center'>First Class Seats</TableCell>
                     <TableCell align='center'>Total price ($)</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
                   <TableRow key={row._uid}>
-                    <TableCell component='th' scope=''>
-                      {row.noOfEconomy}
-                    </TableCell>
-                    <TableCell>{row.noOfBusiness}</TableCell>
+                    <TableCell align='center'>{row.noOfEconomy}</TableCell>
+                    <TableCell align='center'>{row.noOfBusiness}</TableCell>
                     <TableCell align='center'>{row.noOfFirstClass}</TableCell>
                     <TableCell align='center'></TableCell>
                   </TableRow>
-                </TableBody>{' '}
+                </TableBody>
               </Table>
             </Box>
           </Collapse>
--- frontend/src/components/admin/ViewFlights.js
+++ frontend/src/components/admin/ViewFlights.js
@@ -2,51 +2,78 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
+  Box,
   Table,
   TableBody,
   TableCell,
   TableContainer,
+  TableFooter,
   TableHead,
   TableRow,
+  TablePagination,
   Paper,
 } from '@material-ui/core';
 import FlightCard from './FlightCard';
 
 const ViewFlights = (props) => {
   const [data, setData] = useState();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
   useEffect(() => {
     axios
       .get('http://localhost:8081/admin/search')
       .then((res) => {
-        console.log(res.data);
         setData(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label='collapsible table'>
+    <TableContainer component={Paper} style={{ margin: '5% 0%' }}>
+      <Table>
         <TableHead>
           <TableRow>
             <TableCell />
-            <TableCell>Flight Number</TableCell>
+            <TableCell align='left'>Flight Number</TableCell>
             <TableCell align='left'>From</TableCell>
             <TableCell align='left'>Duration</TableCell>
             <TableCell align='left'>To</TableCell>
             <TableCell align='center'>Departure Date</TableCell>
             <TableCell align='left'>Arrival Date</TableCell>
-            <TableCell align='center'>Departure Date</TableCell>
+            <TableCell align='center'></TableCell>
           </TableRow>
         </TableHead>
         {data ? (
           <TableBody>
-            {data.map((row) => (
-              <FlightCard key={row.name} row={row} />
-            ))}
+            {data
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => (
+                <FlightCard key={row._id} row={row} />
+              ))}
           </TableBody>
         ) : null}
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              count={data ? data.length : 0}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   );
