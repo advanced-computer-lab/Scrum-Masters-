7efb85f44bc5eb155c7e79e23ccebd58586ea537
doniasharaf <doniasharaf18@gmail.com>
2021-11-05 01:00:06 +0200

    virtual field for number of seats and navbar design done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TopBar from "./components/layout/navigation/TopBar";
 import { BrowserRouter as Router } from "react-router-dom";
-
 
 function App() {
   return (
     <Router>
       <div className="App">
         <TopBar />
-        <div className="container">
-        </div>
+        <div className="container"></div>
       </div>
     </Router>
   );
