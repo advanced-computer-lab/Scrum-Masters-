21ce33e50db94539a5d4ac86b2ebf8de78443f1f
ahmedserryy <serry1393@gmail.com>
2021-11-05 23:57:05 +0200

    Pagination added in ViewFlights

--- frontend/src/components/admin/ViewFlights.js
+++ frontend/src/components/admin/ViewFlights.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
   Table,
   TableBody,
+  TableFooter,
+  TablePagination,
   TableCell,
   TableContainer,
   TableHead,
@@ -11,23 +13,25 @@ import {
   tableCellClasses,
   styled,
   Paper,
-} from "@mui/material";
-import FlightCard from "./FlightCard";
-import { Container } from "react-bootstrap";
+} from '@mui/material';
+import FlightCard from './FlightCard';
+import { Container } from 'react-bootstrap';
 
 const ViewFlights = (props) => {
   const [data, setData] = useState();
-  
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: "#5390d9",
-    color: theme.palette.common.white,
-  },
-}));
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: '#5390d9',
+      color: theme.palette.common.white,
+    },
+  }));
 
   useEffect(() => {
     axios
-      .post("http://localhost:8081/admin/search")
+      .post('http://localhost:8081/admin/search')
       .then((res) => {
         console.log(res.data);
         setData(res.data);
@@ -35,35 +39,44 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
       .catch((err) => console.log(err));
   }, []);
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
-    <TableContainer component={Paper} style={{ marginBottom: "20px" }}>
-      <Table aria-label="collapsible table">
+    <TableContainer component={Paper} style={{ marginBottom: '20px' }}>
+      <Table aria-label='collapsible table'>
         <TableHead>
           <TableRow>
             <StyledTableCell />
-            <StyledTableCell align="center" style={{ fontWeight: "bolder" }}>
+            <StyledTableCell align='center' style={{ fontWeight: 'bolder' }}>
               Flight Number
             </StyledTableCell>
-            <StyledTableCell align="center" style={{ fontWeight: "bolder" }}>
+            <StyledTableCell align='center' style={{ fontWeight: 'bolder' }}>
               From
             </StyledTableCell>
             {/* <TableCell align="left">Duration</TableCell> */}
-            <StyledTableCell align="center" style={{ fontWeight: "bolder" }}>
+            <StyledTableCell align='center' style={{ fontWeight: 'bolder' }}>
               To
             </StyledTableCell>
-            <StyledTableCell align="center" style={{ fontWeight: "bolder" }}>
+            <StyledTableCell align='center' style={{ fontWeight: 'bolder' }}>
               Departure Date
             </StyledTableCell>
-            <StyledTableCell align="center" style={{ fontWeight: "bolder" }}>
+            <StyledTableCell align='center' style={{ fontWeight: 'bolder' }}>
               Arrival Date
             </StyledTableCell>
-            <StyledTableCell align="center" style={{ fontWeight: "bolder" }}>
-              {" "}
+            <StyledTableCell align='center' style={{ fontWeight: 'bolder' }}>
+              {' '}
               Duration
             </StyledTableCell>
             <StyledTableCell
-              align="center"
-              style={{ fontWeight: "bolder" }}
+              align='center'
+              style={{ fontWeight: 'bolder' }}
             ></StyledTableCell>
             {/* <TableCell align="center" style={{ fontWeight: "bolder" }}>
               Delete Flight
@@ -72,11 +85,25 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
         </TableHead>
         {data ? (
           <TableBody>
-            {data.map((row) => (
-              <FlightCard key={row.name} row={row} />
-            ))}
+            {data
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => (
+                <FlightCard key={row._id} row={row} />
+              ))}
           </TableBody>
         ) : null}
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              count={data ? data.length : 0}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   );
