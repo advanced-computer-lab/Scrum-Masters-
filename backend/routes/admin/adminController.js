7deea04739045254195998f3f0178293c0d6d997
gityoussefsameh <76009741+gityoussefsameh@users.noreply.github.com>
2021-11-05 20:32:24 +0200

    Delete backend

--- backend/routes/admin/adminController.js
+++ backend/routes/admin/adminController.js
@@ -14,15 +14,12 @@ router.get("/search", async (req, res) => {
     res.json({ message: err });
   }
 });
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 router.post("/createflight",async(req, res)=>{
 const flight=new Flight({flightNumber:req.params.flightNumber});
-=======
 router.get("/create/:flightNumber/:departureTime/:arrivalTime/:departureDate/:arrivalDate/:departureAirport/:arrivalAirport/:noOfEconomy/:noOfBusiness/:noOfSeats", (req,res) => {
 const flight=new Flight({flightNumber:req.params.flightNumber, departureTime:req.params.departureTime, arrivalTime:req.params.arrivalTime,  departureDate: req.params.departureDate, arrivalDate: req.params.arrivalDate, departureAirport: req.params.departureAirport, arrivalAirport: req.params.arrivalAirport, noOfEconomy: req.params.noOfEconomy, noOfBusiness:req.params.noOfBusiness, noOfSeats: req.params.noOfSeats});
->>>>>>> maram
+
 try{
    flight.save();
   res.send("WOHOOOOO")
@@ -31,14 +28,11 @@ catch(err){
   console.log(err);
 }
 
-<<<<<<< HEAD
 });
 
->>>>>>> maram
 
-=======
+
 })
->>>>>>> maram
 router.patch("/update/:id", async (req, res) => {
   Flight.findByIdAndUpdate(req.params.id, req.body, { new: true })
     .then((result) => {
