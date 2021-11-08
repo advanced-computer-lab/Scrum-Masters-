850a1b9c62b0307312e32661a8386e2190016d32
ahmedserryy <serry1393@gmail.com>
2021-11-08 23:50:11 +0200

    master pulled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import TopBar from "./components/layout/navigation/TopBar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Schedule from "./pages/admin/Schedule";
-import FlightCreation from "./pages/admin/FlightCreation";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TopBar from './components/layout/navigation/TopBar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Schedule from './pages/admin/Schedule';
+import FlightCreation from './pages/admin/FlightCreation';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <TopBar />
         <Switch>
-          <Route exact path={["/", "/schedule"]} component={Schedule}></Route>
-          <Route exact path="/addFlight" component={FlightCreation}></Route>
+          <Route exact path={['/', '/schedule']} component={Schedule}></Route>
+          <Route exact path='/addFlight' component={FlightCreation}></Route>
         </Switch>
       </div>
     </Router>
