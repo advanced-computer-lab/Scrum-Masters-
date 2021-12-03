5e0129b7cfb48720c3b813ef57ad4e4459259c8a
ahmedserryy <serry1393@gmail.com>
2021-12-03 22:15:46 +0200

    seif merged and input to Maram and Donia done

--- frontend/src/pages/user/signed/BookingPage.js
+++ frontend/src/pages/user/signed/BookingPage.js
@@ -136,55 +136,6 @@ const BookingPage = (props) => {
       </ColorlibStepIconRoot>
     );
   }
-  // const res = {
-  //   flights: [
-  //     {
-  //       economy: {
-  //         baggageAllowance: 2,
-  //         noOfSeats: 78,
-  //         childPrice: 0,
-  //         adultPrice: 10,
-  //         availableSeats: 78,
-  //       },
-  //       business: {
-  //         baggageAllowance: 2,
-  //         noOfSeats: 5,
-  //         childPrice: 0,
-  //         adultPrice: 0,
-  //         availableSeats: 5,
-  //       },
-  //       firstClass: {
-  //         baggageAllowance: 3,
-  //         noOfSeats: 5,
-  //         childPrice: 0,
-  //         adultPrice: 0,
-  //         availableSeats: 5,
-  //       },
-  //       _id: '61a3e0ec766320f267156a54',
-  //       flightNumber: 128,
-  //       departureTime: '16:04',
-  //       arrivalTime: '10:04',
-  //       departureDate: '2021-11-01T00:00:00.000Z',
-  //       arrivalDate: '2021-11-02T00:00:00.000Z',
-  //       departureAirport: 'JPN',
-  //       arrivalAirport: 'JFK',
-  //       __v: 0,
-  //       noOfSeats: 88,
-  //       availableSeats: 88,
-  //       duration: '18h 0m',
-  //       id: '61a3e0ec766320f267156a54',
-  //     },
-  //   ],
-  //   details: {
-  //     noOfAdults: 3,
-  //     noOfChildren: 0,
-  //     departureAirport: 'JPN',
-  //     arrivalAirport: 'JFK',
-  //     departureDate: '2021-11-01T00:00:00.000Z',
-  //     arrivalDate: '2021-11-02T00:00:00.000Z',
-  //     cabin: 'economy',
-  //   },
-  // };
 
   const steps = [
     'Enter Passengers Details',
