f598ec2ba3fb98ff9e6138592141bee191637a3d
ahmedserryy <serry1393@gmail.com>
2021-12-03 18:28:20 +0200

    seif merged and seatmap to be fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import Schedule from './pages/admin/Schedule';
 import FlightCreation from './pages/admin/FlightCreation';
 import SelectSeat from './pages/user/signed/SelectSeat';
 import BookingPage from './pages/user/signed/BookingPage';
-import FlightReservation from './components/user/FlightReservation';
 
 function App() {
   const user = true;
@@ -17,14 +16,11 @@ function App() {
         <Switch>
           <Route exact path={['/', '/schedule']} component={Schedule}></Route>
           <Route exact path='/addFlight' component={FlightCreation}></Route>
-          <Route exact path='/selectSeat' component={SelectSeat}></Route>
-          <Route exact path='/user' component={BookingPage}></Route>
-          <Route exact path='/flights' component={FlightReservation}></Route>
         </Switch>
         {user && (
           <Switch>
             <Route exact path='/selectSeat' component={SelectSeat}></Route>
-            <Route exact path='/user' component={LandingPage}></Route>
+            <Route exact path='/user' component={BookingPage}></Route>
           </Switch>
         )}
       </div>
