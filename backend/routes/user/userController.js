47a603adc25cd037a0af4eaebb04d1ddc2d05afc
gityoussefsameh <76009741+gityoussefsameh@users.noreply.github.com>
2021-12-05 05:07:39 +0200

    SSS

--- backend/routes/user/userController.js
+++ backend/routes/user/userController.js
@@ -236,7 +236,11 @@ router.delete("/delete/reservation/:id", (req, res) => {
     .catch((err) => res.status(404).json({ error: "No such a Reservation" }));
 })
 //user
-
+router.get("/reservations/:id", (req, res) => {
+  Reservation.find({userId : req.params.id})
+    .then((result) => res.json(result))
+    .catch((err) => console.log(err));
+});
 router.get("/profile/:id", async (req, res) => {
   User.findById(req.params.id)
     .then((result) => {
