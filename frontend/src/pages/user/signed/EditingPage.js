0ccc10077f3a1e503c80168647becf12e867ef9a
seifmaged31 <seifmaged2000@gmail.com>
2021-12-23 22:04:24 +0200

    before pulling

--- frontend/src/pages/user/signed/EditingPage.js
+++ frontend/src/pages/user/signed/EditingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import EditReservationButton from '../../../components/user/existing/buttons/EditReservationButton';
 
 const EditingPage = () => {
-  return <EditReservationButton />;
+  return <EditReservationButton resId="61b0a36e2173f72667abb54a"/>;
 };
 
 export default EditingPage;
