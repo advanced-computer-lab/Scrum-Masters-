14979ed750bd7c2d21fa36586f4e7ada480ab9bf
maram <marambenamer@gmail.com>
2021-12-24 20:43:22 +0200

    centering of insertFlight textfields.

--- frontend/src/components/admin/forms/InsertFlight.js
+++ frontend/src/components/admin/forms/InsertFlight.js
@@ -97,7 +97,7 @@ const InsertFlight = () => {
         <Stack
           direction="row"
           spacing={5}
-          style={{ marginTop: "10px", marginLeft: "30%", marginRight: "35%" }}
+          style={{ marginTop: "10px", marginLeft: "20%", marginRight: "35%" }}
         >
           <Stack direction="column" spacing={3} style={{ marginTop: "10px" }}>
             <div>
@@ -284,7 +284,7 @@ const InsertFlight = () => {
             </div>
           </Stack>
           
-          <Stack> 
+          <Stack direction="column" spacing={3} style={{ marginTop: "10px" }}> 
           <div>
               <TextField
                 required
